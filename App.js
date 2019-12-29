0e0d40e06952519a949e45a24c67c206669551c0
LeonShimazaki <48808986+LeonShimazaki@users.noreply.github.com>
2019-12-30 01:22:52 +1300

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import productsData from "./vschoolProducts"
+import Product from "./Product"
+
+function App() {
+    const exampleinfo = productsData.map()
+  return (
+    <div>
+        {exampleinfo}
+    </div>
+  )
+}
+
+export default App
